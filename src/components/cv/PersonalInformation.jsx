0043499d10e173adbb9f3b38c25b3930a7089af1
Sämir Hamadi <samir.hamadi@elisa.fi>
2016-10-02 22:37:28 +0300

    Initial working axios call

--- src/components/cv/PersonalInformation.jsx
+++ src/components/cv/PersonalInformation.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
 
-const json = {
-    "name": "Pekka Peloton",
-    "address": {
-        "street": "Mallikatu 1",
-        "zipcode": "33200",
-        "city": "Tampere"
-    },
-    "phoneNumber": "+358 12 345 6789",
-    "dateOfBirth": "12. elokuuta 1982"
-};
-
 export default React.createClass({
     render() {
         return (<div className="personalInformation">
-            <div className="leftColumn">{json.name}</div><div className="rightColumn">CURRICULUM VITAE</div>
-            <div className="leftColumn">{json.address.street}</div>
-            <div className="leftColumn">{json.address.zipcode} {json.address.city}</div>
-            <div className="leftColumn">{json.phoneNumber}</div>
-            <div className="leftColumn">{json.dateOfBirth}</div>
+            <div className="leftColumn">{this.props.data.name}</div><div className="rightColumn">CURRICULUM VITAE</div>
+            <div className="leftColumn">{this.props.data.address.street}</div>
+            <div className="leftColumn">{this.props.data.address.zipcode} {this.props.data.address.city}</div>
+            <div className="leftColumn">{this.props.data.phoneNumber}</div>
+            <div className="leftColumn">{this.props.data.dateOfBirth}</div>
         </div>);
     }
 });
