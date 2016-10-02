f8bdc34e5d7145bef09760b4958833ce718bdf28
Sämir Hamadi <samir.hamadi@elisa.fi>
2016-10-02 12:08:53 +0300

    Initial router

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default React.createClass({
+    render: function() {
+        return <div>
+            <p>This is the login page...</p>
+        </div>;
+
+    }
+});
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route} from 'react-router';
 import Site from './components/Site';
+import Cv from './components/cv/Cv';
+import Login from './components/login/Login'
 
-ReactDOM.render(
-    <Site />,
+ReactDOM.render((
+    <Router>
+        <Route path="/" component={Site} />
+        <Route path="/cv" component={Cv} />
+        <Route path="/login" component={Login} />
+    </Router>),
     document.getElementById('app')
 );
 
