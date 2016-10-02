38015a52bac56184a1fbaec4d94c0f192a72e9e5
Sämir Hamadi <samir.hamadi@elisa.fi>
2016-10-02 22:44:22 +0300

    Move server side loading functionality to the component

--- src/components/cv/Cv.jsx
+++ src/components/cv/Cv.jsx
@@ -1,40 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router'
 import PersonalInformation from './PersonalInformation';
-import axios from 'axios';
-
-const json = {
-    "name": "Pekka Peloton - NON SERVER",
-    "address": {
-        "street": "Mallikatu 1",
-        "zipcode": "33200",
-        "city": "Tampere"
-    },
-    "phoneNumber": "+358 12 345 6789",
-    "dateOfBirth": "12. elokuuta 1982"
-};
 
 export default React.createClass({
-    getInitialState: function() {
-        return { data: json };
-    },
-    loadPersonalInformation() {
-        axios.get('http://localhost:8090/contents/personals')
-            .then((response) => {
-                const json = response.data;
-                console.log(json);
-                this.setState({ data: json });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    },
-    componentDidMount() {
-        this.loadPersonalInformation();
-    },
     render() {
         return (<div className="cv">
-                    <PersonalInformation data={this.state.data} />
+                    <PersonalInformation />
                </div>);
     }
 });
--- src/components/cv/PersonalInformation.jsx
+++ src/components/cv/PersonalInformation.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
+import axios from 'axios';
+
+const json = {
+    "name": "Pekka Peloton - NON SERVER",
+    "address": {
+        "street": "Mallikatu 1",
+        "zipcode": "33200",
+        "city": "Tampere"
+    },
+    "phoneNumber": "+358 12 345 6789",
+    "dateOfBirth": "12. elokuuta 1982"
+};
 
 export default React.createClass({
+    getInitialState: function() {
+        return { data: json };
+    },
+    loadPersonalInformation() {
+        axios.get('http://localhost:8090/contents/personals')
+            .then((response) => {
+                const json = response.data;
+                console.log(json);
+                this.setState({ data: json });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+    componentDidMount() {
+        this.loadPersonalInformation();
+    },
     render() {
         return (<div className="personalInformation">
-            <div className="leftColumn">{this.props.data.name}</div><div className="rightColumn">CURRICULUM VITAE</div>
-            <div className="leftColumn">{this.props.data.address.street}</div>
-            <div className="leftColumn">{this.props.data.address.zipcode} {this.props.data.address.city}</div>
-            <div className="leftColumn">{this.props.data.phoneNumber}</div>
-            <div className="leftColumn">{this.props.data.dateOfBirth}</div>
+            <div className="leftColumn">{this.state.data.name}</div><div className="rightColumn">CURRICULUM VITAE</div>
+            <div className="leftColumn">{this.state.data.address.street}</div>
+            <div className="leftColumn">{this.state.data.address.zipcode} {this.state.data.address.city}</div>
+            <div className="leftColumn">{this.state.data.phoneNumber}</div>
+            <div className="leftColumn">{this.state.data.dateOfBirth}</div>
         </div>);
     }
 });
