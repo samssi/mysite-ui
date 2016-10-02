14c9415af90923d2abec1e4bad70dbd5cacf8793
Sämir Hamadi <samir.hamadi@elisa.fi>
2016-10-02 22:52:38 +0300

    Map to components own domain instead of using json almost directly

--- src/components/cv/PersonalInformation.jsx
+++ src/components/cv/PersonalInformation.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import axios from 'axios';
 
-const json = {
-    "name": "Pekka Peloton - NON SERVER",
-    "address": {
-        "street": "Mallikatu 1",
-        "zipcode": "33200",
-        "city": "Tampere"
-    },
-    "phoneNumber": "+358 12 345 6789",
-    "dateOfBirth": "12. elokuuta 1982"
-};
-
 export default React.createClass({
     getInitialState: function() {
-        return { data: json };
+        return { name: '',
+                 street: '',
+                 zipCodeAndCity: '',
+                 phoneNumber: '',
+                 dateOfBirth: ''
+        };
     },
     loadPersonalInformation() {
         axios.get('http://localhost:8090/contents/personals')
             .then((response) => {
                 const json = response.data;
-                console.log(json);
-                this.setState({ data: json });
+                this.setState({
+                    name: json.name,
+                    street: json.address.street,
+                    zipCodeAndCity: json.address.zipcode + ' ' + json.address.city,
+                    phoneNumber: json.phoneNumber,
+                    dateOfBirth: json.dateOfBirth
+                    });
             })
             .catch((error) => {
                 console.log(error);
@@ -32,11 +31,11 @@ export default React.createClass({
     },
     render() {
         return (<div className="personalInformation">
-            <div className="leftColumn">{this.state.data.name}</div><div className="rightColumn">CURRICULUM VITAE</div>
-            <div className="leftColumn">{this.state.data.address.street}</div>
-            <div className="leftColumn">{this.state.data.address.zipcode} {this.state.data.address.city}</div>
-            <div className="leftColumn">{this.state.data.phoneNumber}</div>
-            <div className="leftColumn">{this.state.data.dateOfBirth}</div>
+            <div className="leftColumn">{this.state.name}</div><div className="rightColumn">CURRICULUM VITAE</div>
+            <div className="leftColumn">{this.state.street}</div>
+            <div className="leftColumn">{this.state.zipCodeAndCity}</div>
+            <div className="leftColumn">{this.state.phoneNumber}</div>
+            <div className="leftColumn">{this.state.dateOfBirth}</div>
         </div>);
     }
 });
