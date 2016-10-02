59c1b33d376f36787ff5b911a7476aedb2ec836b
Sämir Hamadi <samir.hamadi@elisa.fi>
2016-10-02 16:56:21 +0300

    Render json to the screen

--- src/components/cv/PersonalInformation.jsx
+++ src/components/cv/PersonalInformation.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
+const json = {
+    "name": "Pekka Peloton",
+    "address": {
+        "street": "Mallikatu 1",
+        "zipcode": "33200",
+        "city": "Tampere"
+    },
+    "phoneNumber": "+358 12 345 6789",
+    "dateOfBirth": "12. elokuuta 1982"
+};
+
 export default React.createClass({
     render() {
         return (<div className="name">
-            <p>John Doe</p>
-            <p>Curriculum Vitae</p>
+            <p>{json.name}</p>
+            <p>{json.address.street}</p>
+            <p>{json.address.zipcode} {json.address.city}</p>
+            <p>{json.phoneNumber}</p>
+            <p>{json.dateOfBirth}</p>
         </div>);
     }
 });
