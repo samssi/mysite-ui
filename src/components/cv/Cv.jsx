851b947ea9c77e53e3274ea43ab20c2d7c8e2839
Sämir Hamadi <samir.hamadi@gmail.com>
2016-10-19 20:19:45 +0300

    Add animation with global css

--- src/components/cv/Cv.jsx
+++ src/components/cv/Cv.jsx
@@ -8,12 +8,30 @@ const cvBlock = {
     'margin-left': '30px'
 };
 
+const cursor = {
+    'margin-top': '10px',
+    'width': '20px',
+    'height': '20px',
+    'background-color': 'white',
+    'animation-name': 'blink',
+    '-webkit-animation-name': 'blink',
+    '-webkit-animation-duration': '1s',
+    '-webkit-animation-iteration-count': 'infinite',
+    'animation-iteration-count': 'infinite',
+    'animation-duration': '1s',
+    '-webkit-animation-timing-function': 'linear',
+    'animation-timing-function': 'linear'
+};
+
 class Cv extends React.Component {
     render() {
-        return (<div style={cvBlock}>
-                    <PersonalInformation />
-                    <Experience />
-               </div>);
+        return (<div>
+                    <div style={cvBlock}>
+                        <PersonalInformation />
+                        <Experience />
+                    </div>
+                    <div style={cursor}>&nbsp;</div>
+                </div>);
     }
 }
 
