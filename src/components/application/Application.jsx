a3f64e4351adde68a92fdcb124253edfe47ca074
Sämir Hamadi <samir.hamadi@gmail.com>
2017-01-05 02:36:37 +0200

    Initial loading of some application data

--- src/components/application/Application.jsx
+++ src/components/application/Application.jsx
@@ -3,10 +3,49 @@ import ReactDOM from 'react-dom';
 import Cursor from '../style/Cursor'
 import Letter from './Letter'
 
+import axiosFactory from '../factory/axiosFactory';
+const axios = axiosFactory.createAxiosContentRestClient();
+
+const greetingField = {
+
+};
+
 class Application extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            greeting: '',
+            signatureTitle: '',
+            signature: '',
+            paragraphs: []
+
+        }
+    }
+
+    loadApplication() {
+        console.log('foo');
+        axios.get('/private/contents/applications')
+            .then((response) => {
+                const json = response.data;
+                this.setState({
+                    greeting: json.greeting,
+                    signatureTitle: json.signatureTitle,
+                    signature: json.signature,
+                    paragraphs: json.paragraphs
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    componentDidMount() {
+        this.loadApplication();
+    }
+
     render() {
         return (<div>
-                    <Letter />
+                    <div style={greetingField}><span>{this.state.greeting}</span></div>
                   </div>
         );
     }
