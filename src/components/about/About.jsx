9edc979ba07f55a08228f9c02441973fa6fc5450
Sämir Hamadi <samir.hamadi@gmail.com>
2017-02-11 20:46:58 +0200

    Initial About loading

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import axiosFactory from '../factory/axiosFactory';
+const axios = axiosFactory.createAxiosContentRestClient();
 
 const about = {
     color: 'white'
 };
 
 class About extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            document: ''
+        }
+    }
+
+    loadAbout() {
+        axios.get("/private/contents/about")
+            .then((response) => {
+                const json = response.data;
+                this.setState({
+                    document: json
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    componentDidMount() {
+        this.loadAbout();
+    }
+
     render() {
-        return (<div style={about}>About...</div>)
+        const document = this.state.document;
+        return (<div style={about}>
+            <div>{document.header}</div>
+            <div>{document.header}</div>
+        </div>);
     }
 }
 
