2c4a224ae89dc87f548ea65a2e6b5806653c3cbf
samssi <samir.hamadi@gmail.com>
2018-11-12 20:42:35 +0200

    Fix session expiration redirect

--- src/components/factory/axiosFactory.js
+++ src/components/factory/axiosFactory.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 
-import {BrowserRouter} from "react-router-dom";
-
-export function createAxiosAuthRestClient() {
+function createAxiosAuthRestClient() {
     return axios.create({
         baseURL: __AUTH_API_URL__
     });
 }
 
-export function createAxiosContentRestClient() {
+function createAxiosContentRestClient() {
     const client = axios.create({
         baseURL: __CONTENT_API_URL__
     });
@@ -16,10 +14,16 @@ export function createAxiosContentRestClient() {
         return response;
     }, function (error) {
         if (error.response.status === 401) {
-            console.info("Token expired. Removing token...");
-            sessionStorage.removeItem("jwt");
-            BrowserRouter.push("/login");
+            throwOut();
         }
     });
     return client;
 }
+
+function throwOut() {
+    console.info("Token expired. Removing token...");
+    sessionStorage.removeItem("jwt");
+    document.location.href = "/";
+}
+
+export {createAxiosAuthRestClient, createAxiosContentRestClient};
