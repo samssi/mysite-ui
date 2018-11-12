39b89995a64912c36285720bad278ad684b48acb
samssi <samir.hamadi@gmail.com>
2018-11-12 18:22:25 +0200

    Change menu order to fit the flow of the site better

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -32,8 +32,8 @@ const liAStyle = {
 class Menu extends React.Component {
     render() {
         return (<ul role="nav" style={ulStyle}>
-            <MenuItem to="/cv" text="CV" protected="true"/>
             <MenuItem to="/application" text="APPLICATION" protected="true"/>
+            <MenuItem to="/cv" text="CV" protected="true"/>
             <MenuItem to="/portfolio" text="PORTFOLIO" protected="true"/>
             <MenuItem to="/about" text="ABOUT" protected="true"/>
             <LoginItem />
